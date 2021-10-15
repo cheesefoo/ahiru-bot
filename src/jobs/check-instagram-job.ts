539c136984b02287bf820ed9bf7edc500e35bc10
cheesefoo <cheesefoo666@gmail.com>
2021-10-15 13:18:41 -0400

    put in asyncs

--- src/jobs/check-instagram-job.ts
+++ src/jobs/check-instagram-job.ts
@@ -49,57 +49,58 @@ export class CheckInstagram implements Job {
     }
 
     public async buildEmbed(res) {
-        console.log("res\n");
-        console.log(res);
+        // console.log("res\n");
+        // console.log(res);
         let url = await this.get_last_publication_url(res);
         let embed = {
             color: 0xec054c,
             title: `New post from @${this.username}`,
             url: `https://www.instagram.com/p/${url}/`,
 
-            description: this.get_description_photo(res),
-            image: { "url": this.get_last_thumb_url(res) },
+            description: await this.get_description_photo(res),
+            image: { "url": await this.get_last_thumb_url(res) },
             thumbnail: {
-                "url": this.get_last_thumb_url(res)
+                "url": await this.get_last_thumb_url(res)
             }
         };
         return embed;
 
     }
 
-    public get_user_fullname(html) {
-        return html.json()["graphql"]["user"]["full_name"]
+    public async get_user_fullname(html) {
+        return await html.json()["graphql"]["user"]["full_name"]
     }
 
 
-    public get_total_photos(html): Number {
-        return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
+    public async get_total_photos(html): Number {
+        return await html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
     }
 
 
     public async get_last_publication_url(html) {
-        let json = await html.json();
-        console.log("json\n");
-        console.log(json);
-        let json2 = json["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"];
-        console.log("json2\n");
-        console.log(json2);
-        return json;
+        // let json = await html.json();
+        // console.log("json\n");
+        // console.log(json);
+        // let json2 = json["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"];
+        // console.log("json2\n");
+        // console.log(json2);
+        // return json;
+        return await html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"];
     }
 
 
-    public get_last_photo_url(html) {
-        return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"]
+    public async get_last_photo_url(html) {
+        return await html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"]
     }
 
 
-    public get_last_thumb_url(html) {
-        return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"]
+    public async get_last_thumb_url(html) {
+        return await html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"]
     }
 
 
-    public get_description_photo(html) {
-        return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    public async get_description_photo(html) {
+        return await html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
     }
 
 }
