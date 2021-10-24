6f673b22747810568126b0904955e114b92648e1
cheesefoo <cheesefoo666@gmail.com>
2021-10-24 17:25:57 -0400

    .

--- src/commands/subtitle-command.ts
+++ src/commands/subtitle-command.ts
@@ -1,9 +1,8 @@
-import translate from 'deepl';
 import { Message, MessageAttachment } from 'discord.js';
 import { LangCode } from '../models/enums';
 import { EventData } from '../models/internal-models';
 import { Lang } from '../services';
-import { ApiUtils, MessageUtils } from '../utils';
+import { MessageUtils } from '../utils';
 import { Command } from './command';
 import Canvas from 'canvas';
 
@@ -26,16 +25,16 @@ export class SubtitleCommand implements Command {
 
     public async execute(msg: Message, args: string[], data: EventData): Promise<void> {
         let template = 'https://cdn.discordapp.com/attachments/825378176993722378/901942196293492836/template.png';
-        let text;
+        let text: string;
         if (args.length === 2) {
-            await MessageUtils.send(msg.channel, Lang.getEmbed('displayEmbeds.deepLHelp', data.lang()));
+            await MessageUtils.send(msg.channel, Lang.getEmbed('displayEmbeds.subtitleHelp', data.lang()));
             return;
         }
 
         text = args.slice(2).reduce((prev, cur, _index, _array) => {
             return prev + cur;
         });
-        const canvas = Canvas.createCanvas(700, 250);
+        const canvas = Canvas.createCanvas(1920, 1080);
         const ctx = canvas.getContext('2d');
 
         const background = await Canvas.loadImage(template);
@@ -49,10 +48,10 @@ export class SubtitleCommand implements Command {
             let fontSize = 70;
 
             do {
-                // Assign the font to the context and decrement it so it can be measured again
+                
                 context.font = `${fontSize -= 10}px sans-serif`;
-                // Compare pixel width of the text to the canvas minus the approximate avatar size
-            } while (context.measureText(text).width > canvas.width - 300);
+                
+            } while (context.measureText(text).width > canvas.width - 100);
 
             // Return the result to use in the actual canvas
             return context.font;
@@ -61,7 +60,7 @@ export class SubtitleCommand implements Command {
         ctx.font = applyText(canvas, text);
         ctx.fillStyle = '#ffffff';
         ctx.textAlign = "center";
-        this.wrapText(ctx, text, canvas.width / 2.5, canvas.height / 1.8, canvas.width - 1000, 18)
+        this.wrapText(ctx, text, canvas.width / 2, canvas.height *0.8, canvas.width - 1000, 18)
 
 
 
