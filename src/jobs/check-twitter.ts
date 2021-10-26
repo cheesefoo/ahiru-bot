23961c453a27e8daedb4e5bc74e4c6856c5ac14b
cheesefoo <cheesefoo666@gmail.com>
2021-10-26 12:10:47 -0400

    add ui

--- src/jobs/check-twitter.ts
+++ src/jobs/check-twitter.ts
@@ -1,12 +1,11 @@
 // const Twitter = require('twitter-v2');
 import Twitter from 'twitter-v2';
-import fetch, { HeaderInit, Response } from 'node-fetch';
-import { BotSite } from '../models/config-models';
-import { HttpService, Lang, Logger } from '../services';
-import { MessageUtils, ShardUtils, DatabaseUtils } from '../utils';
+import { Response } from 'node-fetch';
+import { Logger } from '../services';
+import { MessageUtils, DatabaseUtils } from '../utils';
 import { Job } from './job';
 
-import { Channel, Client, Collection, Guild, GuildMember, TextChannel } from 'discord.js';
+import { Client, TextChannel } from 'discord.js';
 import { TwitterSpaceUtils } from '../utils/twitter-space-utils';
 let Config = require('../../config/config.json');
 let Logs = require('../../lang/logs.json');
@@ -14,15 +13,18 @@ let baseEndpoint = "https://api.twitter.com/2/spaces/search"
 // let testId = '1449090654542434308'
 let testId = '313289241'
 let subaId = '1027853566780698624'
+let uimamaId = '69496975'
+let broadcastChannel = '722257568361087057';
+let broadcastChannel2 = '825378176993722378';
+let checks = [[subaId, broadcastChannel], [uimamaId, broadcastChannel2]]
 export class CheckTwitter implements Job {
   public name = 'Check Twitter';
   public schedule: string = Config.jobs.checkTwitter.schedule;
   public log: boolean = Config.jobs.checkTwitter.log;
 
   constructor(private client: Client) { }
-
   //subastream
-  private broadcastChannel = '722257568361087057';
+
   //test
   // private broadcastChannel = '825378176993722378';
 
@@ -35,35 +37,40 @@ export class CheckTwitter implements Job {
       bearer_token: process.env.twitter_token
 
     });
-    let endPt = `https://api.twitter.com/2/spaces/by/creator_ids?user_ids=${subaId}`;
-    let res = await twitter.get<Response>("spaces/by/creator_ids", { user_ids: subaId });
+    
+    checks.forEach(async ([id, channel]) => {
 
-    //There is a live space
-    if (res["meta"]["result_count"] != 0) {
-      let spaceId = res["data"][0].id;
-      let state = res["data"][0].state;
-      try {
-        //Check if we've seen it already
-        if (await DatabaseUtils.CheckIfExists("SPACES", spaceId)) {
-          Logger.info(Logs.info.spacesold.replace('{SC}', spaceId));
-        } else {
-          //New, post to discord
-          let embed = await this.buildEmbed(spaceId);
-          let ch: TextChannel = this.client.channels.cache.get(this.broadcastChannel) as TextChannel;
-          await MessageUtils.send(ch, { embeds: [embed] });
-          let metadata = await TwitterSpaceUtils.GetMetadata(spaceId);
-          let url = await TwitterSpaceUtils.GetURL(metadata);
-          await MessageUtils.send(ch, "@venndiagram#7498\n" + "`" + url + "`");
 
-          await DatabaseUtils.Insert("SPACES", spaceId, url.toString());
+      let endPt = `https://api.twitter.com/2/spaces/by/creator_ids?user_ids=${id}`;
+      let res = await twitter.get<Response>("spaces/by/creator_ids", { user_ids: id });
+
+      //There is a live space
+      if (res["meta"]["result_count"] != 0) {
+        let spaceId = res["data"][0].id;
+        let state = res["data"][0].state;
+        try {
+          //Check if we've seen it already
+          if (await DatabaseUtils.CheckIfExists("SPACES", spaceId)) {
+            Logger.info(Logs.info.spacesold.replace('{SC}', spaceId));
+          } else {
+            //New, post to discord
+            let embed = await this.buildEmbed(spaceId);
+            let ch: TextChannel = this.client.channels.cache.get(channel) as TextChannel;
+            await MessageUtils.send(ch, { embeds: [embed] });
+            let metadata = await TwitterSpaceUtils.GetMetadata(spaceId);
+            let url = await TwitterSpaceUtils.GetURL(metadata);
+            await MessageUtils.send(ch, "@venndiagram#7498\n" + "`" + url + "`");
+
+            await DatabaseUtils.Insert("SPACES", spaceId, url.toString());
+          }
+        } catch (error) {
+          Logger.error(Logs.error.job.replace('{JOB}', 'CheckTwitter'), error);
         }
-      } catch (error) {
-        Logger.error(Logs.error.job.replace('{JOB}', 'CheckTwitter'), error);
-      }
-    } else {
-      Logger.info(Logs.info.nospace);
+      } else {
+        Logger.info(Logs.info.nospace);
 
-    }
+      }
+    });
     Logger.info(Logs.info.jobCompleted.replace('{JOB}', 'CheckTwitter'));
   }
 
